72941d735934aa299f5133fc2e42e947535051d0
Egor Levchenko <50330458+furtivite@users.noreply.github.com>
2025-05-16 22:12:04 +0300

    feat: finish catalogue on main page (#4)
    * feat: add theming
    
    * feat: add Short card component
    
    * feat: add catalogue on  start page
    
    * feat: add catalogue on  start page
    
    * fix: delete second title line
    
    * fix: delete second content line

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,33 @@
 import * as React from "react";
 import { Header } from "./Header.tsx";
+import { Box, Container, Typography, useTheme } from "@mui/material";
 
 interface Props {
   title: string;
   children: React.ReactNode | string;
 }
 
-export const Layout: React.FC<Props> = ({title, children}) => (
+export const Layout: React.FC<Props> = ({title, children}) => {
+  const theme = useTheme();
+  const titleBg = theme.palette.custom.titleBg;
+  const titleColor = theme.palette.custom.titleColor;
+  
+
+  return(
   <>
     <Header />
-    <h1>{title}</h1>
-    {children}
+    {/* Блок с заголовком */}
+    <Box sx={{ width: "100%", bgcolor: titleBg, py: 3 }}>
+      <Container maxWidth="xl">
+        <Typography variant="h4" component="h1" sx={{ color: titleColor }}>
+          {title}
+        </Typography>
+      </Container>
+    </Box>
+
+    {/* Основной контент */}
+    <Box sx={{ py: 4 }}>
+      <Container maxWidth="xl">{children}</Container>
+    </Box>
   </>
-);
+)};
