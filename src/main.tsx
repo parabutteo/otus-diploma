ae126ea8298b71f3e6f17208e9de29a75906310d
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2025-05-16 23:42:11 +0300

    feat: add apollo client and its config

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import App from './app/App.tsx'
-import { Provider } from "react-redux";
-import "./app/i18n"; // i18n init
-import { CssBaseline } from "@mui/material";
-import {CustomThemeProvider} from "./app/ThemeContext.tsx";
+import App from './app/App.tsx';
+import { Provider } from 'react-redux';
+import './app/i18n'; // i18n init
+import { CssBaseline } from '@mui/material';
+import { CustomThemeProvider } from './app/ThemeContext.tsx';
 import store from './store/store.ts';
+import { Client as ApolloClient } from './client/ApolloClient';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <CustomThemeProvider>
-        <CssBaseline />
-        <BrowserRouter>
-        <App />
-        </BrowserRouter>
-      </CustomThemeProvider>
-    </Provider>
+    <ApolloClient>
+      <Provider store={store}>
+        <CustomThemeProvider>
+          <CssBaseline />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </CustomThemeProvider>
+      </Provider>
+    </ApolloClient>
   </StrictMode>,
-)
+);
