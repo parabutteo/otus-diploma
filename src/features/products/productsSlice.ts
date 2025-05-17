1bc91e6d5c3b1fc834406474b6ec2c146f5156d9
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2025-05-17 11:36:42 +0300

    feat: add GQL catalog

--- src/features/products/productsSlice.ts
+++ src/features/products/productsSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import createRandomProductImage from '../createRandomProductImage';
 
 interface Product {
   id: string;
@@ -10,40 +9,7 @@ interface Product {
   details: string;
 }
 
-const initialState: Product[] = [
-  {
-    id: '1',
-    title: 'Рубашка',
-    details: 'Рубашка с принтом',
-    price: 1200,
-    image: createRandomProductImage(3),
-    category: 'Футболки, рубашки',
-  },
-  {
-    id: '2',
-    title: 'Кардиган',
-    details: 'Вязаный кардиган',
-    price: 2800,
-    image: createRandomProductImage(1),
-    category: 'Футболки, рубашки',
-  },
-  {
-    id: '3',
-    title: 'Джемпер',
-    details: 'Крупная вязка',
-    price: 1700,
-    image: createRandomProductImage(2),
-    category: 'Футболки, рубашки',
-  },
-  {
-    id: '4',
-    title: 'Жилетка',
-    details: 'Базовая жилетка',
-    price: 2100,
-    image: createRandomProductImage(4),
-    category: 'Футболки, рубашки',
-  },
-];
+const initialState: Product[] = [];
 
 const productsSlice = createSlice({
   name: 'products',
@@ -61,11 +27,8 @@ const productsSlice = createSlice({
     removeProduct(state, action: { payload: string }) {
       return state.filter((item) => item.id !== action.payload);
     },
-    addRandomProducts(state, action: { payload: Product[] }) {
-      state.push(...action.payload);
-    },
   },
 });
 
-export const { addProduct, editProduct, removeProduct, addRandomProducts } = productsSlice.actions;
+export const { addProduct, editProduct, removeProduct } = productsSlice.actions;
 export default productsSlice.reducer;
