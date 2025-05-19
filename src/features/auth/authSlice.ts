c722300ec7f8980d99a98928dff1b5eee3e2340d
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2025-05-19 12:38:11 +0300

    feat: remove rtk data profile reducer

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -3,23 +3,11 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 interface AuthState {
   profileId: string;
   token: string | null;
-  profile: {
-    id: string;
-    name: string;
-    email: string;
-    aboutMe: string;
-  } | null;
 }
 
-const loadProfileFromStorage = () => {
-  const storedProfile = localStorage.getItem('profile');
-  return storedProfile ? JSON.parse(storedProfile) : null;
-};
-
 const initialState: AuthState = {
-  profileId: localStorage.getItem('pid') || null,
+  profileId: localStorage.getItem('pid') || '',
   token: localStorage.getItem('token') || null,
-  profile: loadProfileFromStorage(),
 };
 
 const authSlice = createSlice({
@@ -29,31 +17,18 @@ const authSlice = createSlice({
     setToken(state, action: PayloadAction<string>) {
       state.token = action.payload;
       localStorage.setItem('token', action.payload);
-      const profile = {
-        id: 'user1',
-        name: 'Фродо Бэггинс',
-        email: 'mail@mail.ru',
-        aboutMe: 'Вот что я скажу о себе:',
-      };
-      state.profile = profile;
-      localStorage.setItem('profile', JSON.stringify(profile));
     },
     setProfileID(state, action: PayloadAction<string>) {
       state.profileId = action.payload;
       localStorage.setItem('pid', action.payload);
     },
-    updateProfile(state, action: PayloadAction<AuthState['profile']>) {
-      state.profile = action.payload;
-      localStorage.setItem('profile', JSON.stringify(action.payload));
-    },
     logout: (state) => {
       state.token = null;
-      state.profileId = null;
-      state.profile = null;
+      state.profileId = '';
       localStorage.removeItem('token');
     },
   },
 });
 
-export const { setToken, setProfileID, updateProfile, logout } = authSlice.actions;
+export const { setToken, setProfileID, logout } = authSlice.actions;
 export default authSlice.reducer;
